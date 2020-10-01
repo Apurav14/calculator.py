aa0669c6d07afabfd68c1bac6ac2ed9c3449f46a
Apurav Mahajan <62143278+Apurav14@users.noreply.github.com>
2020-10-02 02:33:53 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,13 @@
 while(1):
     a=float(input("enter your first value:- "))
     b=float(input("enter your second value:- "))
-    operator =input("Enetr the operator: ")
+    operator =input("Enter the operator: ")
     if (operator == "+"):
-        print("The adition of",a,"and",b,"is",a+b)
+        print("The addition of",a,"and",b,"is",a+b)
     elif (operator =="-"):
         print("the subtraction of",a,"and",b,"is",a-b)
     elif (operator =="*"):
-        print("the multipaction of",a,"and",b,"is",a*b)
+        print("the multiplication of",a,"and",b,"is",a*b)
     elif (operator =="/"):
         print("the division of",a,"and",b,"is",a/b)
     else:
